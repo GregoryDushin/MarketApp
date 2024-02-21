6caba773f6a749d43b651615092d7e55a9615f73
GrigoryDushin <g.s.dushin@gmail.com>
2024-02-21 19:30:55 +0100

    Start

---
+++ MarketApp/MarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketAppApp.swift
+//  MarketApp
+//
+//  Created by Григорий Душин on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CategoryView()
+        }
+    }
+}
---
+++ MarketApp/Model/Category.swift
@@ -0,0 +1,24 @@
+//
+//  Framework.swift
+//  MarketApp
+//
+//  Created by Григорий Душин on 21.02.2024.
+//
+
+import Foundation
+
+struct Category: Hashable {
+    let name: String
+    let imageName: String
+}
+
+struct MockData {
+    
+    static let categories = [
+        Category(name: "Пекарни", imageName: "Frame 1"),
+        Category(name: "Фастфуд", imageName: "Frame 2"),
+        Category(name: "Азиатская кухня", imageName: "Frame 3"),
+        Category(name: "Супы", imageName: "Frame 4")
+    ]
+}
+
